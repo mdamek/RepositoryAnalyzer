ef3908c0a51c3e5a394611bc8920539b44f68ee0
MarcinDamek <marcind70@gmail.com>
2019-12-17 16:04:21 +0100

    Add all pipeline to ps

--- CreateCodeStatistics.ps1
+++ CreateCodeStatistics.ps1
@@ -10,3 +10,11 @@ $codeLinesCalculions.replace($FolderPath, $root) > 'OutputsFiles\CodeStatisticsO
 Write-Host 'Calculating lines of code...'
 & 'AnalyzeManager\build\AnalyzeManager.exe' 'OutputsFiles\CodeStatisticsOutput.txt' $FolderPath
 Write-Host 'All statistics of'$solutionPath ' solution are ready.'
+if(!(Test-Path 'node_modules'))
+{
+    npm install http-server
+}
+Start-Job {npx http-server}
+Write-Host 'Waiting for http server start'
+Start-Sleep -s 3
+Start-Process http://127.0.0.1:8080/AnalyzeReport/GitAnalyzeReport
